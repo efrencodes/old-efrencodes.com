b07ed41ba55a1eb4abb5168b280866b4f891c660
efrenmartinez <developer.emartinez@gmail.com>
2021-08-21 22:36:10 -0500

    :wrench: Update config Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,22 @@
 module.exports = {
+	parser: "babel-eslint",
 	extends: [
 		//...
 		'plugin:@next/next/recommended',
 		'prettier'
-	]
+	],
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+		jest: true,
+	},
+	rules: {},
+	parserOptions: {
+		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module', // Allows for the use of imports
+		ecmaFeatures: {
+		  jsx: true, // Allows for the node of JSX
+		},
+	},
 }
