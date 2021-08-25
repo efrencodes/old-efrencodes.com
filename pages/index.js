8a3ab2c47eda67e4dca11f8c704ed939b3334b3a
efrenmartinez <developer.emartinez@gmail.com>
2021-08-25 11:12:30 -0500

    :mag: Add SEO

--- pages/index.js
+++ pages/index.js
@@ -27,12 +27,31 @@ export default function Home(/* { posts, response } */) {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Front-end engineer | Efrén Martínez Rodríguez</title>
+				<title>Efrén Martínez Rodríguez -- Front-end engineer</title>
 				<meta
 					name="description"
 					content="I´m FrontEnd Enginner from Queretaro, Mexico. I code, write and build stuff on internet."
 				/>
 				<meta charset="UTF-8" />
+				<meta
+					name="keywords"
+					content="technology,software,web,development,fullstack,software engineer,developer,jamstack"
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://efrenmartinez.dev" />
+				<meta property="og:title" content="website" />
+				<meta property="og:type" content="Efrén Martínez Rodríguez -- Front-end engineer" />
+				<meta property="og:description" content="I´m FrontEnd Enginner from Queretaro, Mexico. I code, write and build stuff on internet." />
+				<meta property="og:image" content="" />
+				// Twitter Card
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://efrenmartinez.dev" />
+				<meta property="twitter:title" content="Efrén Martínez Rodríguez -- Front-end engineer" />
+				<meta property="twitter:description" content="I´m FrontEnd Enginner from Queretaro, Mexico. I code, write and build stuff on internet." />
+				<meta property="twitter:image" content="" />
+				<meta property="twitter:creator" content="@efrenmartinezmx" />
+				<meta property="twitter:site" content="@efrenmartinezmx" />
+				<meta property="twitter:image:alt" content="Efrén Martínez Rodríguez -- Front-end engineer" />
 				<link
 					rel="icon"
 					href="https://res.cloudinary.com/efrenmartinez/image/upload/v1608834240/efrenmartinez.dev/favicon_lvhhjc.svg"
