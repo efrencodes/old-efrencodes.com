1a916a6e8628afd351c00bbf7b26224105213023
efrencodes <developer.emartinez@gmail.com>
2021-10-04 14:41:27 -0500

    :sparkles: Add page 404

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,32 @@
 import Link from 'next/link'
+import Footer from '../components/Footer'
+import styles from '../styles/Custom.module.css'
 
-const Custom404 = () => {
+export const getStaticProps = async () => {
+	const URL = 'https://www.efrenmartinez.dev/'
+	const socialMedia = await fetch(`${URL}api/socialMedia`, {
+		method: 'GET'
+	}).then((res) => res.json())
+
+	return {
+		props: {
+			socialMedia
+		}
+	}
+}
+
+const Custom404 = ({ socialMedia }) => {
 	return (
-		<>
-			<h1>404 - Page Not Found</h1>
-			<Link href="/">
-				<a>Ir al inicio</a>
-			</Link>
-		</>
+		<div className="container">
+			<main className={styles.Main}>
+				<h2 className={styles.Subtitle}>404</h2>
+				<h1 className={styles.Title}>Lost in space?</h1>
+				<Link href="/">
+					<a className={styles.Anchor}>Ir al Home</a>
+				</Link>
+			</main>
+			<Footer socialMedia={socialMedia} />
+		</div>
 	)
 }
 export default Custom404
