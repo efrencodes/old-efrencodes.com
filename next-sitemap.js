c5893f4886175b2c8c86bc27277a68cf7977a59c
efrenmartinez <developer.emartinez@gmail.com>
2021-10-23 16:16:49 -0500

    :wrench: Update configuration next-sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,23 @@
 module.exports = {
 	siteUrl: 'https://efrencodes.com/',
 	generateRobotsTxt: false,
-	changefreq: 'daily',
+	changefreq: 'weekly',
 	priority: 0.7,
-	sitemapSize: 5000
+	sitemapSize: 5000,
+	generateRobotsTxt: true,
+	exclude: ['/about'],
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: 'Googlebot',
+				allow: '/',
+				disallow: '/about'
+			},
+			{
+				userAgent: '*',
+				allow: '/',
+				disallow: '/about'
+			}
+		]
+	}
 }
